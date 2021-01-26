f6eea107aa719dd57965abc3b809078a9294d035
Justinas Ruslys <jjustinas.ruslys@gmail.com>
2021-01-26 20:40:10 +0200

    created a class for holding video data

---
+++ Video.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace video_tracker_v2
+{
+    public class Video
+    {
+        public string Path { get; set; }
+
+        public Video(string path)
+        {
+            this.Path = path;
+        }
+    }
+}
