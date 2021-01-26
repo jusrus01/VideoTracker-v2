69f1ddc172e243724e94e14b37924e361946b711
Justinas Ruslys <jjustinas.ruslys@gmail.com>
2021-01-26 13:29:32 +0200

    created a class which will handle data saving and loading

---
+++ DataManager.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.IO;
+
+namespace video_tracker_v2
+{
+    public static class DataManager
+    {
+        public static string Path { get; set; }
+
+        public static string[] LoadCategories()
+        {
+            if (File.Exists(Path))
+            {
+                string[] categories = File.ReadAllLines(Path);
+                // check if files exists
+                List<string> validCategories = new List<string>();
+                foreach(string category in categories)
+                {
+                    if(File.Exists(category))
+                    {
+                        validCategories.Add(category);
+                    }
+                }
+                if (validCategories.Count == 0)
+                    return null;
+
+                return validCategories.ToArray<string>();
+            }
+            return null;
+        }
+
+        public static void SaveCategory(string categoryPath)
+        {
+            File.AppendAllText(Path, categoryPath);
+        }
+    }
+}
