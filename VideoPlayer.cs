8b358668c8b2c600fd6901888d3761e57ad7c701
Justinas Ruslys <jjustinas.ruslys@gmail.com>
2021-01-28 11:02:42 +0200

    created a list of valid extensions

--- VideoPlayer.cs
+++ VideoPlayer.cs
@@ -11,6 +11,56 @@ namespace video_tracker_v2
 {
     class VideoPlayer
     {
+        public static string ValidExtensions = ".asx " +
+            ".dts" +
+            ".gxf" +
+            ".m2v" +
+            ".m3u" +
+            ".m4v" +
+            ".mpeg1" +
+            ".mpeg2" +
+            ".mts" +
+            ".mxf" +
+            ".ogm" +
+            ".pls" +
+            ".bup" +
+            ".a52" +
+            ".acc" +
+            ".b4s" +
+            ".cue" +
+            ".divx" +
+            ".dv" +
+            ".flv" +
+            ".m1v" +
+            ".m2ts" +
+            ".mkv" +
+            ".mov" +
+            ".mpeg4" +
+            ".oma" +
+            ".spx" +
+            ".ts" +
+            ".vlc" +
+            ".vob" +
+            ".xspf" +
+            ".dat" +
+            ".bin" +
+            ".ifo" +
+            ".part" +
+            ".3g2" +
+            ".avi" +
+            ".mpeg" +
+            ".mpg" +
+            ".flac" +
+            ".m4a" +
+            ".mp1" +
+            ".ogg" +
+            ".wav" +
+            ".xm" +
+            ".3gp" +
+            ".wmv" +
+            ".wma" +
+            ".mp4";
+
         public MediaPlayer mPlayer { get; set; }
         public Video currentVideo;
         public Media currentMedia;
